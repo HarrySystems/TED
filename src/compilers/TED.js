41c56921aab4df188aacbe5de38392b1b3073ce2
LUIZ GUILHERME DE PAULA SANTOS <LGPSANT@corp.ad.emb>
2017-09-22 14:42:57 -0300

    Added stylus support

--- src/compilers/TED.js
+++ src/compilers/TED.js
@@ -79,6 +79,52 @@ var transpilers = [
 				}
 			);
 		}
+	},
+
+	{
+		name: "stylus",
+		ext: "stylus",
+		ini: function() {
+			Premise.partial("stylus.min.js");
+		},
+		elements: {
+			inline: "style",
+			external: "link"
+		},
+		transpile: function(source, target) {
+			sass.compile(
+				source,
+				{ indentedSyntax: true},
+				function(result) {
+					// Something was wrong
+					// if (result.line && result.message) {
+					// 	showErrors('css', [
+					// 		{ lineNumber: result.line - 1, message: result.message }
+					// 	]);
+					// }
+					//console.log(result);
+					target.innerHTML = result.text;
+					// console.log(result.text);
+				}
+			);
+
+			stylus(code).render(function(error, result) {
+				// if (error) {
+				// 	window.err = error;
+				// 	// Last line of message is the actual message
+				// 	var tempArr = error.message.split('\n');
+				// 	tempArr.pop(); // This is empty string in the end
+				// 	showErrors('css', [
+				// 		{
+				// 			lineNumber: +error.message.match(/stylus:(\d+):/)[1] - 298,
+				// 			message: tempArr.pop()
+				// 		}
+				// 	]);
+				// }
+
+				target.innerHTML = result
+			});
+		}
 	}
 
 ];
